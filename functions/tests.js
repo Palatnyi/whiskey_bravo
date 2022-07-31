6949cefd33361f21b0b8d53b1e326ea7cee196e9
palatnyi <andrey.palatnyi@gmail.com>
2022-07-31 14:05:40 +0300

    bot v1

--- functions/tests.js
+++ functions/tests.js
@@ -2,13 +2,13 @@ const axios = require('axios');
 const updateJson = require('../testPositionsUpdate_4.json');
 
 let index = 2700;
-updateJson[2700].data.alertState = 'start';
+// updateJson[index].data.alertState = 'start';
 const intervalId = setInterval(() => {
   if (index === updateJson.length) {
     clearInterval(intervalId);
   } else {
-    // axios.post('http://localhost:5001/whiskeybravo-9aa4d/us-central1/app/dedrone', updateJson[index]);
-    axios.post('http://localhost:5001/dedrone', updateJson[index]);
+    axios.post('http://localhost:3000/dedrone', updateJson[index]);
+    // axios.post('http://localhost:5001/dedrone', updateJson[index]);
     console.log(index)
     
     index += 1;
