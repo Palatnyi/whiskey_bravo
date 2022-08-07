671e53c848516d729e034228669510f2554fbfac
palatnyi <andrey.palatnyi@gmail.com>
2022-08-07 14:17:11 +0300

    tests

--- functions/dedrone-pull.js
+++ functions/dedrone-pull.js
@@ -4,280 +4,352 @@ import cron from 'node-cron';
 import TelegramBot from 'node-telegram-bot-api';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-let isPulling;
-const chat_id = '-1001678724997';
-const dedroneToken = 'BedRxVUpuiZytmsqLnjEcxMhcsypWJhJ';
-const droneToken = '5401170277:AAGXh_DUBGLqJCJAEVVnHDR9LY2KFrbPXng';
-const remoteToken = '5307737139:AAGsvSzJWtGjpAf_miSXcIBaCViOVzH0SjI';
-const pullUrl = 'https://ukr2.cloud.dedrone.com/api/1.0/systemstate';
-const testurl = 'http://localhost:3000/test-pull';
 
-const bots = {
-  drone: new TelegramBot(droneToken),
-  remote: new TelegramBot(remoteToken)
-};
+class FlightActivityTracker {
+  constructor() {
+    this._dbCache;
+    this.isPulling;
+    this._dbName = 'dedrone';
+    this._collectionName = 'alerts'
+    this.chat_id = '-1001678724997';
+    this._pullFrequency = "*/5 * * * * *"
+    this._testurl = 'http://localhost:3000/test-pull';
+    this._dedroneToken = 'BedRxVUpuiZytmsqLnjEcxMhcsypWJhJ';
+    this._pullUrl = 'https://ukr2.cloud.dedrone.com/api/1.0/systemstate';
+    this._mongoUri = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.wiznw.mongodb.net/?retryWrites=true&w=majority';
 
-let dbCache;
-const getDBCache = () => dbCache;
-const setDBCache = (cache) => dbCache = cache;
-const mongoUri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.wiznw.mongodb.net/?retryWrites=true&w=majority";
-const connectToMongo = async (uri) => {
-  const cache = getDBCache();
-  if (cache && cache.client) return Promise.resolve(cache);
+    this._bots = {
+      drone: new TelegramBot('5401170277:AAGXh_DUBGLqJCJAEVVnHDR9LY2KFrbPXng'),
+      remote: new TelegramBot('5307737139:AAGsvSzJWtGjpAf_miSXcIBaCViOVzH0SjI')
+    };
 
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    this._telegramMessagesCount = {};
 
-  try {
-    await client.connect();
-    console.log('CONNECTED TO MONGODB');
-  } catch (e) {
-    console.log('FAILED CONNECT TO MONGO', e);
-    return { error: true, msg: 'failed connect to mongoDB' }
-  }
-  setDBCache({ client })
-
-  return Promise.resolve({ client });
-}
-
-const closeConnection = async () => {
-  const cache = getDBCache();
-  if (cache && cache.client) {
-    cache.client.close();
-    setDBCache({})
-  }
-}
-
-const getCollection = async (client, dbName, collectionName) => {
-  if (!dbName) {
-    console.log('No DB provided');
-    return { error: true, msg: 'No DB provided' };
   }
 
-  if (!collectionName) {
-    console.log('NoCollection name provided');
-    return { error: true, msg: 'No DB provided' }
+  setCollectionName = name => this._collectionName = name;
+
+  getDBCache = () => this._dbCache;
+
+  setDBCache = (cache) => this._dbCache = cache;
+
+  connectToMongo = async () => {
+    const cache = this.getDBCache();
+    if (cache && cache.client) return Promise.resolve(cache);
+
+    const client = new MongoClient(this._mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+    try {
+      await client.connect();
+      console.log('CONNECTED TO MONGODB');
+    } catch (e) {
+      console.log('FAILED CONNECT TO MONGO', e);
+      return { error: true, msg: 'failed connect to mongoDB' }
+    }
+    this.setDBCache({ client })
+
+    return Promise.resolve({ client });
   }
 
-  return await client.db(dbName).collection(collectionName);
-}
+  closeConnection = async () => {
+    const cache = this.getDBCache();
+    if (cache && cache.client) {
+      cache.client.close();
+      this.setDBCache({})
+    }
+  }
 
-const getExtendedSearchQuery = ({ detectionId, identification, detectionType }) => {
-  const keys = ['manufacturer', 'protocol', 'detectionType', 'label'];
+  getCollection = async (client, dbName, collectionName) => {
+    if (!dbName) {
+      console.log('No DB provided');
+      return { error: true, msg: 'No DB provided' };
+    }
 
-  const extendedQuery = keys.map(key => {
-    const value = identification[key];
+    if (!collectionName) {
+      console.log('NoCollection name provided');
+      return { error: true, msg: 'No DB provided' }
+    }
+
+    return await client.db(dbName).collection(collectionName);
+  }
+
+  getExtendedSearchQuery = ({ detectionId, identification, detectionType }) => {
+    const keys = ['manufacturer', 'protocol', 'detectionType', 'label'];
+
+    const extendedQuery = keys.map(key => {
+      const value = identification[key];
+
+      return {
+        $or: [{
+          [`${detectionType}.identification.${key}`]: { $eq: value }
+        }, {
+          [`${detectionType}.identification.${key}`]: { $type: 9 }
+        }, {
+          [`${detectionType}.identification.${key}`]: { $exists: false }
+        },]
+      }
+    });
+
+    const and = extendedQuery.concat([
+      { $and: [{ [`${detectionType}.timestampWindow`]: { $exists: true } }, { [`${detectionType}.timestampWindow`]: { $gte: Date.now() } }] },
+    ])
 
     return {
       $or: [{
-        [`${detectionType}.identification.${key}`]: { $eq: value }
+        detectionId
       }, {
-        [`${detectionType}.identification.${key}`]: { $type: 9 }
-      }, {
-        [`${detectionType}.identification.${key}`]: { $exists: false }
-      },]
+        $and: and
+      }]
     }
-  });
-
-  const and = extendedQuery.concat([
-    { $and: [{ [`${detectionType}.timestampWindow`]: { $exists: true } }, { [`${detectionType}.timestampWindow`]: { $gte: Date.now() } }] },
-  ])
-
-  return {
-    $or: [{
-      detectionId
-    }, {
-      $and: and
-    }]
-  }
-}
-
-const isPositionEquals = ({ oldPosition = {}, newPosition = {} }) => {
-  const oldCoordinates = _.pick(oldPosition, ['latitude', 'longitude']);
-  const newCoordinates = _.pick(newPosition, ['latitude', 'longitude']);
-
-  return _.isEqual(oldCoordinates, newCoordinates);
-}
-
-const getWelcomeMessage = (detectionType) => {
-  let message = `Запілінговано оператора дрону 🥸 🏴‍☠`;
-
-  if (detectionType === 'drone') {
-    message =
-      `Запілінговано дрон 🚨🚨🚨
-Надсилаю координати...`
   }
 
-  return message;
-}
+  isPositionEquals = ({ oldPosition = {}, newPosition = {} }) => {
+    const oldCoordinates = _.pick(oldPosition, ['latitude', 'longitude']);
+    const newCoordinates = _.pick(newPosition, ['latitude', 'longitude']);
 
-const requestSystemState = async ({ url, token }) => {
-  let response;
-  try {
-    console.log('requesting /systemstate...');
-    const { data } = await axios.get(url, { headers: { 'Dedrone-Auth-Token': token } });
-    response = data;
-  } catch (e) {
-    console.log('error')
-    response = { error: true, msg: 'Failed to request dedrone API' };
+    return _.isEqual(oldCoordinates, newCoordinates);
   }
 
-  return response;
-}
+  getWelcomeMessage = (detectionType) => {
+    let message = `Запілінговано оператора дрону 🥸 🏴‍☠`;
 
-const pullAlerts = async () => {
-  if (isPulling) {
-    return
+    if (detectionType === 'drone') {
+      message =
+        `Запілінговано дрон 🚨🚨🚨
+  Надсилаю координати...`
+    }
+
+    return message;
   }
 
-  console.log('pulling...');
-  isPulling = true;
 
-  const { client } = await connectToMongo(mongoUri);
-  const collection = await getCollection(client, 'dedrone', 'alerts');
-  let systemState = await requestSystemState({ url: testurl, token: dedroneToken });
+  requestSystemState = async (opt = {}) => {
+    const url = opt.url || this._pullUrl;
+    const token = opt.token || this._dedroneToken;
 
-  if (systemState.error) {
-    console.log(systemState.msg);
-    isPulling = false;
-    return { msg: 'Failed to request dedrone API' }
+    let response;
+    try {
+      console.log('requesting /systemstate...');
+      const { data } = await axios.get(url, { headers: { 'Dedrone-Auth-Token': token } });
+      response = data;
+    } catch (e) {
+      console.log('error')
+      response = { error: true, msg: 'Failed to request dedrone API' };
+    }
+
+    return response;
   }
 
-  const alerts = _.get(systemState, 'data.currentAlertState.alerts', [])
+  telegramOperationCount = (detectionType, internalId, type) => {
+    const typeCounter = this._telegramMessagesCount[detectionType];
 
-  if (!alerts.length) {
-    console.log('No alerts in systemState response');
-    isPulling = false;
-    return { msg: 'No alerts in systemState response' }
-  }
-
-  for (const alert of alerts) {
-    const { internalId, detections = [] } = alert;
-
-    if (!detections.length) {
-      console.log('no detections in the alert');
-      continue
-    }
-
-    const singleDetection = detections.find(det => det.detectionId === internalId);
-
-    if (!singleDetection) {
-      console.log('no singleDetection in the detections array');
-      continue
-    }
-
-    let { detectionId } = singleDetection;
-    let { positions = [] } = singleDetection;
-    const { identification } = singleDetection;
-
-    const { detectionType } = identification;
-
-    if (!detectionType) {
-      console.log('device (remote/drone) has not been recognised yet');
-      continue
-    }
-
-    if (!positions.length) {
-      console.log('no positions in singleDetection object');
-      continue
-    }
-
-    positions = _.last(positions);
-
-    const newPosition = {
-      longitude: positions[0],
-      latitude: positions[1]
-    }
-
-    const currentDoc = await collection.findOne(getExtendedSearchQuery({ detectionId, identification, detectionType }));
-    const oldPosition = _.get(currentDoc, `${detectionType}.position`, {});
-
-    if (isPositionEquals({ oldPosition, newPosition })) {
-      console.log('positions equals');
-      continue
-    }
-
-    const timestamp = Date.now();
-    const timestampWindow = timestamp + 120000;
-    detectionId = _.get(currentDoc, 'detectionId', detectionId);
-
-    let set = {
-      detectionId,
-      [`${detectionType}.timestamp`]: timestamp,
-      [`${detectionType}.position`]: newPosition,
-      [`${detectionType}.identification`]: identification,
-      [`${detectionType}.timestampWindow`]: timestampWindow,
-    }
-
-    const bot = bots[detectionType];
-    const { latitude, longitude } = newPosition;
-
-    if (currentDoc && currentDoc[detectionType] && currentDoc[detectionType].liveLocationStarted) {
-      try {
-        await bot.editMessageLiveLocation(
-          latitude,
-          longitude,
-          {
-            chat_id,
-            message_id: currentDoc[detectionType].message_id,
-            horizontal_accuracy: 1
-          }
-        );
-        console.log('EDIT', { latitude, longitude });
-      } catch (e) {
-        console.log('edit errror', e);
-      }
+    if (!typeCounter) {
+      typeCounter = {
+        count: 1,
+        messagesMeta: [{ internalId, type, detectionType }],
+      };
     } else {
-      try {
-
-        await bot.sendMessage(chat_id, getWelcomeMessage(detectionType));
-
-        const { message_id } = await bot.sendLocation(
-          chat_id,
-          latitude,
-          longitude, {
-          live_period: 4000,
-          protect_content: true
-        });
-
-        set = {
-          ...set,
-          [`${detectionType}.message_id`]: message_id,
-          [`${detectionType}.liveLocationStarted`]: true,
-        }
-
-        console.log('Live location started', { latitude, longitude }, message_id)
-      } catch (e) {
-        console.error('START LIVE LOCATION FAILED', { latitude, longitude }, e);
-      }
+      typeCounter = {
+        count: typeCounter.count + 1,
+        messagesMeta: typeCounter.messagesMeta.concat([{ internalId, type, detectionType }])
+      };
     }
 
-    await collection.findOneAndUpdate({ detectionId }, { $set: set }, { upsert: true });
+    this._telegramMessagesCount[detectionType] = { ...typeCounter };
   }
 
-  isPulling = false;
+  setAlertStatus = (internalId, msg) => {
+    this._alertsStatuses = {
+      [internalId]: { msg }
+    }
+  }
+
+  pullAlerts = async () => {
+    if (this.isPulling) {
+      return
+    }
+
+    this._alertsStatuses = {};
+    this._telegramMessagesCount = {};
+    
+    console.log('pulling...');
+    this.isPulling = true;
+
+    const { client } = await this.connectToMongo();
+    const collection = await this.getCollection(client, this._dbName, this._collectionName);
+    let systemState = await this.requestSystemState({ url: this._testurl, token: this._dedroneToken });
+
+    if (systemState.error) {
+      console.log(systemState.msg);
+      this.isPulling = false;
+      return { ...systemState };
+    }
+
+    const alerts = _.get(systemState, 'data.currentAlertState.alerts', [])
+
+    if (!alerts.length) {
+      console.log('No alerts in systemState response');
+      this.isPulling = false;
+      return { error: false, msg: 'No alerts in systemState response', ...this._telegramMessagesCount }
+    }
+
+    let alertsStatuses = {};
+
+    for (const alert of alerts) {
+      const { internalId, detections = [] } = alert;
+
+      if (!detections.length) {
+        alertsStatuses = {
+          ...alertsStatuses,
+          [internalId]: {
+            msg: 'no detections in the alert'
+          },
+        }
+        console.log('no detections in the alert');
+        continue
+      }
+
+      const singleDetection = detections.find(det => det.detectionId === internalId);
+
+      if (!singleDetection) {
+        alertsStatuses = {
+          ...alertsStatuses,
+          [internalId]: {
+            msg: 'no singleDetection in the detections array'
+          },
+        }
+        console.log('no singleDetection in the detections array');
+        continue
+      }
+
+      let { detectionId } = singleDetection;
+      let { positions = [] } = singleDetection;
+      const { identification } = singleDetection;
+
+      const { detectionType } = identification;
+
+      if (!detectionType) {
+        alertsStatuses = {
+          ...alertsStatuses,
+          [internalId]: {
+            msg: 'device (remote/drone) not recognized'
+          },
+        }
+        console.log('device (remote/drone) has not been recognised yet');
+        continue
+      }
+
+      if (!positions.length) {
+        alertsStatuses = {
+          ...alertsStatuses,
+          [internalId]: {
+            msg: 'no positions in singleDetection object'
+          },
+        }
+        console.log('no positions in singleDetection object');
+        continue
+      }
+
+      positions = _.last(positions);
+
+      const newPosition = {
+        longitude: positions[0],
+        latitude: positions[1]
+      }
+
+      const currentDoc = await collection.findOne(this.getExtendedSearchQuery({ detectionId, identification, detectionType }));
+      const oldPosition = _.get(currentDoc, `${detectionType}.position`, {});
+
+      if (this.isPositionEquals({ oldPosition, newPosition })) {
+        console.log('positions equals');
+        continue
+      }
+
+      const timestamp = Date.now();
+      const timestampWindow = timestamp + 120000;
+      detectionId = _.get(currentDoc, 'detectionId', detectionId);
+
+      let set = {
+        detectionId,
+        [`${detectionType}.timestamp`]: timestamp,
+        [`${detectionType}.position`]: newPosition,
+        [`${detectionType}.identification`]: identification,
+        [`${detectionType}.timestampWindow`]: timestampWindow,
+      }
+
+      const bot = this._bots[detectionType];
+      const { latitude, longitude } = newPosition;
+
+      const isEditMessage = currentDoc && currentDoc[detectionType] && currentDoc[detectionType].liveLocationStarted;
+
+      if (isEditMessage) {
+        try {
+          await bot.editMessageLiveLocation(
+            latitude,
+            longitude,
+            {
+              chat_id,
+              message_id: currentDoc[detectionType].message_id,
+              horizontal_accuracy: 1
+            }
+          );
+          console.log('EDIT', { latitude, longitude });
+        } catch (e) {
+          console.log('edit errror', e);
+        }
+      } else {
+        try {
+
+          await bot.sendMessage(chat_id, this.getWelcomeMessage(detectionType));
+
+          const { message_id } = await bot.sendLocation(
+            chat_id,
+            latitude,
+            longitude, {
+            live_period: 4000,
+            protect_content: true
+          });
+
+          set = {
+            ...set,
+            [`${detectionType}.message_id`]: message_id,
+            [`${detectionType}.liveLocationStarted`]: true,
+          }
+
+          console.log('Live location started', { latitude, longitude }, message_id)
+        } catch (e) {
+          console.error('START LIVE LOCATION FAILED', { latitude, longitude }, e);
+        }
+      }
+
+      await collection.findOneAndUpdate({ detectionId }, { $set: set }, { upsert: true });
+
+      const type = isEditMessage ? 'edit' : 'start';
+      this.telegramOperationCount(detectionType, internalId, type);
+    }
+
+    this.isPulling = false;
+
+    return { ...alertsStatuses };
+  }
+
+  start = async () => {
+    await this.connectToMongo();
+
+    this._task = cron.schedule(this._pullFrequency, async () => {
+      await this.pullAlerts();
+    });
+
+    this._task.start();
+  }
+
+  stop = () => {
+    if (this._task) {
+      this._task.stop();
+    }
+  }
 }
 
-const task = cron.schedule("*/5 * * * * *", async () => {
-  await pullAlerts();
-}, { scheduled: false });
-
-
-export default {
-  task,
-  pullAlerts,
-  isPositionEquals,
-  getWelcomeMessage,
-  requestSystemState,
-  getExtendedSearchQuery,
-  getCollection,
-  connectToMongo,
-  mongoUri,
-  dbCache,
-  pullUrl,
-  dedroneToken,
-  closeConnection,
-  getDBCache,
-  setDBCache
-}
+export default FlightActivityTracker
 
 
--- functions/dedrone-pull.test.js
+++ functions/dedrone-pull.test.js
@@ -1,74 +1,180 @@
 import sinon from 'sinon';
 import mocha from 'mocha';
-import assert, { doesNotReject } from 'node:assert';
+import assert from 'node:assert';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-import dedronePull from './dedrone-pull.js';
+import FlightActivityTracker from './dedrone-pull.js';
 import pulledAlertsTest from '../pulledAlertsTest.json' assert { type: 'json'};
-
-
-const { connectToMongo, mongoUri, getCollection, dedroneToken, pullUrl, requestSystemState, closeConnection, getDBCache,
-  setDBCache, pullAlerts } = dedronePull;
 
 const dbName = 'dedrone';
 const collectionName = 'alertsTest';
 
 describe('dedron-pull.js', async () => {
+  let flt;
   const sandbox = sinon.createSandbox();
 
-  beforeEach(() => { 
-
+  beforeEach('new FlightActivityTracker', () => {
+    flt = new FlightActivityTracker();
   })
 
   afterEach(() => {
     sandbox.restore();
+    flt = null;
   });
 
   it('connectToMongo', async () => {
-    let cache = getDBCache();
+    let cache = flt.getDBCache();
     assert.equal(cache, undefined);
-    const { client } = await connectToMongo(mongoUri);
 
+    const { client } = await flt.connectToMongo();
     assert.equal(client.constructor.name, 'MongoClient');
 
-    cache = getDBCache()
+    cache = flt.getDBCache()
     assert.ok(cache);
     assert.ok(cache.client);
     assert.equal(cache.client.constructor.name, 'MongoClient');
 
-    return await closeConnection();
+    return await flt.closeConnection();
 
   });
 
   it('getCollection', async () => {
-    const { client } = await connectToMongo(mongoUri);
+    const { client } = await flt.connectToMongo();
     assert.equal(client.constructor.name, 'MongoClient');
-  
-    const collection = await getCollection(client, dbName, collectionName);
+
+    const collection = await flt.getCollection(client, dbName, collectionName);
     assert.equal(collection.collectionName, collectionName);
-    return await closeConnection();
+    return await flt.closeConnection();
   });
 
-
   it('requestSystemState. Succeded to request DD API', async () => {
-    let systemState = await requestSystemState({ url: pullUrl, token: dedroneToken });
+    let systemState = await flt.requestSystemState();
     assert.equal(systemState.error, undefined)
   });
 
   it('requestSystemState. Failed to request DD API', async () => {
-    let systemState = await requestSystemState({ url: 'http://localhost:3000/ololo', token: 'ololo' });
+    let systemState = await flt.requestSystemState({ url: 'http://localhost:3000/ololo', token: 'ololo' });
     assert.equal(systemState.error, true)
   });
 
-  it('pullAlerts. No alerts in systemState response', async () => {
+  it('pullAlerts. No alerts in the systemState response', async () => {
     const systemState = { data: { currentAlertState: { alerts: [] } } }
-    sinon.replace(dedronePull, "requestSystemState", sinon.fake(() => systemState));
-    
 
-    const result = await pullAlerts();
-    console.log(result);
-    assert.equal(result.error, true);
+    sinon.replace(flt, "requestSystemState", sinon.fake(() => systemState));
+
+    const result = await flt.pullAlerts();
+    assert.equal(flt.isPulling, false);
     assert.equal(result.msg, 'No alerts in systemState response');
+
+  });
+
+  it('pullAlert. Detectionsarray is empty within requestSystemState response', async () => {
+    const systemState = {
+      data: {
+        currentAlertState: {
+          alerts: [{
+            "internalId": 123,
+            "detections": []
+          }]
+        }
+      }
+    };
+
+    sinon.replace(flt, "requestSystemState", sinon.fake(() => systemState));
+    flt.setCollectionName('alertsTest');
+    const result = await flt.pullAlerts();
+    assert.equal(result['123'].msg, 'no detections in the alert');
+
+  });
+
+
+  it('pullAlert. Detections array does not have detecteion related to current alert', async () => {
+    const systemState = {
+      data: {
+        currentAlertState: {
+          alerts: [{
+            "internalId": 123,
+            "detections": [{
+              detectionId: 321
+            }]
+          }]
+        }
+      }
+    };
+
+    sinon.replace(flt, "requestSystemState", sinon.fake(() => systemState));
+    flt.setCollectionName('alertsTest');
+    const result = await flt.pullAlerts();
+    assert.equal(result['123'].msg, 'no singleDetection in the detections array');
+
+  });
+
+  it('pullAlert. Detections array does not have detecteion related to current alert', async () => {
+    const systemState = {
+      data: {
+        currentAlertState: {
+          alerts: [{
+            "internalId": 123,
+            "detections": [{
+              detectionId: 321
+            }]
+          }]
+        }
+      }
+    };
+
+    sinon.replace(flt, "requestSystemState", sinon.fake(() => systemState));
+    flt.setCollectionName('alertsTest');
+    const result = await flt.pullAlerts();
+    assert.equal(result['123'].msg, 'no singleDetection in the detections array');
+
+  });
+
+  it('pullAlert. detectionType(remoote/drone) within one singleDetection has not been recognized', async () => {
+    const systemState = {
+      data: {
+        currentAlertState: {
+          alerts: [{
+            "internalId": 123,
+            "detections": [{
+              detectionId: 123, 
+              "identification": {}
+            }]
+          }]
+        }
+      }
+    };
+
+    sinon.replace(flt, "requestSystemState", sinon.fake(() => systemState));
+    flt.setCollectionName('alertsTest');
+    const result = await flt.pullAlerts();
+    assert.equal(result['123'].msg, 'device (remote/drone) not recognized');
+
+  });
+
+
+  it('pullAlert. Positions array within singleDetection object is empty', async () => {
+    const systemState = {
+      data: {
+        currentAlertState: {
+          alerts: [{
+            "internalId": 123,
+            "detections": [{
+              detectionId: 123, 
+              "identification": {
+                'detectionType': 'remote'
+              }
+            }]
+          }]
+        }
+      }
+    };
+
+    sinon.replace(flt, "requestSystemState", sinon.fake(() => systemState));
+    flt.setCollectionName('alertsTest');
+    const result = await flt.pullAlerts();
+    assert.equal(result['123'].msg, 'no positions in singleDetection object');
+
   });
 
 });
